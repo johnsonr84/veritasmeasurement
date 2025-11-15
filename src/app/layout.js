c4bbb60fc8f5e6db346b741a1eb3c4c79da94fbb
Dev Dork <robertcjohnson1984@gmail.com>
2025-11-14 20:21:54 -0700

    created header and installed hero icons and headlessui deps

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Fixedfooter from '../components/Footer';
+import Header from '../components/Header';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body >
+        <Header />
         <main className="min-h-screen">
           {children}
         </main>
